87b4559fca7b83825a53a2c1e06ca1eec018be14
lorenzop <lorenzo@poixson.com>
2018-06-18 04:41:45 -0400

    removed old ioUtils::OpenFileFromJar() function - using jcl.getResourceAsStream() directly

--- core/src/com/poixson/utils/ioUtils.java
+++ core/src/com/poixson/utils/ioUtils.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 
 import com.poixson.exceptions.RequiredArgumentException;
 
@@ -21,7 +19,8 @@ public final class ioUtils {
 	 * @param fileStr Package path to the file.
 	 * @return InputStream of the open file, or null on failure.
 	 */
-	public static InputStream OpenResource(final Class<? extends Object> clssRef, final String fileStr) {
+	public static InputStream OpenResource(
+			final Class<? extends Object> clssRef, final String fileStr) {
 		if (Utils.isEmpty(fileStr)) throw new RequiredArgumentException("fileStr");
 		final Class<? extends Object> clss = (
 			clssRef == null
@@ -32,29 +31,6 @@ public final class ioUtils {
 			clss.getResourceAsStream(
 				StringUtils.ForceStarts("/", fileStr)
 			);
-		return in;
-	}
-
-
-
-	/**
-	 * Open a file contained in an external jar.
-	 * @param jarFile The jar file containing the file to open.
-	 * @param fileStr Package path to the file.
-	 * @return InputStream of the open file, or null on failure.
-	 * note: The jarFile object must be closed when no longer needed.
-	 * @throws IOException
-	 */
-	public static InputStream OpenFileFromJar(final JarFile jarFile, final String fileStr)
-			throws IOException {
-		if (jarFile == null)        throw new RequiredArgumentException("jarFile");
-		if (Utils.isEmpty(fileStr)) throw new RequiredArgumentException("fileStr");
-		final JarEntry jarEntry =
-			jarFile.getJarEntry(fileStr);
-		if (jarEntry == null)
-			return null;
-		final InputStream in =
-			jarFile.getInputStream(jarEntry);
 		return in;
 	}
 
