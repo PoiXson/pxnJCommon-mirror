7f3d327a2242a776a45814c13f7c98271e8ce102
lorenzop <lorenzo@poixson.com>
2018-09-14 08:31:10 -0400

    added new UnexpectedThreadException class

---
+++ core/src/com/poixson/exceptions/UnexpectedThreadException.java
@@ -0,0 +1,14 @@
+package com.poixson.exceptions;
+
+
+public class UnexpectedThreadException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+
+
+	public UnexpectedThreadException() {
+	}
+
+
+
+}
