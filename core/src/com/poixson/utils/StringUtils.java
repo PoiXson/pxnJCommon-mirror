7f2b0156888e94211c02ca49084818a113c833a7
lorenzop <lorenzo@poixson.com>
2020-05-27 00:29:38 -0400

    moved functions from StringAdvUtils class into StringUtils class

--- core/src/com/poixson/utils/StringUtils.java
+++ core/src/com/poixson/utils/StringUtils.java
@@ -7,8 +7,11 @@ import java.net.URLDecoder;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 
 import com.poixson.exceptions.RequiredArgumentException;
 import com.poixson.tools.Keeper;
@@ -90,6 +93,44 @@ public final class StringUtils {
 
 
 
+//TODO: remove this?
+/*
+	public static String[] BytesToStringArray(final byte[] bytes) {
+		if (bytes == null)     return null;
+		if (bytes.length == 0) return new String[0];
+		final List<String> list = new ArrayList<String>();
+		final int bytesSize = bytes.length;
+		int last = 0;
+		for (int i=0; i<bytesSize; i++) {
+			if (bytes[i] == 0) {
+				if (i - last <= 0) continue;
+				list.add(
+					new String(
+						bytes,
+						last,
+						i - last,
+						CHARSET_ASCII
+					)
+				);
+				last = i+1;
+			}
+		}
+		if (last+1 < bytesSize) {
+			list.add(
+				new String(
+					bytes,
+					last,
+					bytesSize,
+					CHARSET_ASCII
+				)
+			);
+		}
+		return list.toArray(new String[0]);
+	}
+*/
+
+
+
 	// decode string
 	public static String decode(final String raw) {
 		return decode(raw, null, null);
@@ -148,7 +189,7 @@ public final class StringUtils {
 				index++;
 				if (index == multiline ||
 				(index > multiline && line.contains("\n")) ) {
-					final String trimLine = StringUtils.Trim(line, "\n");
+					final String trimLine = Trim(line, "\n");
 					for (final String splitLine : trimLine.split("\n")) {
 						result.add(
 							splitLine.replace("\r",  "")
@@ -479,6 +520,133 @@ if (caseInsensitive) throw new UnsupportedOperationException("UNFINISHED ARGUMEN
 
 
 	// ------------------------------------------------------------------------------- //
+	// build string
+
+
+
+	// add strings with delimiter
+	public static String MergeStrings(final String delim, final String... addThis) {
+		if (Utils.isEmpty(addThis)) throw new RequiredArgumentException("addThis");
+		final String dlm = (Utils.isEmpty(delim) ? null : delim);
+		final StringBuilder buf = new StringBuilder();
+		boolean b = false;
+		for (final String line : addThis) {
+			if (Utils.isEmpty(line)) continue;
+			if (b && dlm != null) {
+				buf.append(dlm);
+			}
+			buf.append(line);
+			if (!b && buf.length() > 0) {
+				b = true;
+			}
+		}
+		return buf.toString();
+	}
+	public static String MergeStrings(final char delim, final String... addThis) {
+		if (Utils.isEmpty(addThis)) throw new RequiredArgumentException("addThis");
+		final StringBuilder buf = new StringBuilder();
+		boolean first = true;
+		for (final String line : addThis) {
+			if (Utils.isEmpty(line)) continue;
+			if (!first)
+				buf.append(delim);
+			buf.append(line);
+			if (first) {
+				if (buf.length() > 0)
+					first = false;
+			}
+		}
+		return buf.toString();
+	}
+
+
+
+	// add objects to string with delimiter
+	public static String MergeObjects(final String delim, final Object... addThis) {
+		if (Utils.isEmpty(addThis)) throw new RequiredArgumentException("addThis");
+		String[] addStrings = new String[ addThis.length ];
+		int index = 0;
+		for (final Object obj : addThis) {
+			addStrings[index] = CastString(obj);
+			index++;
+		}
+		return MergeStrings(delim, addStrings);
+	}
+	public static String MergeObjects(final char delim, final Object... addThis) {
+		if (Utils.isEmpty(addThis)) throw new RequiredArgumentException("addThis");
+		String[] addStrings = new String[ addThis.length ];
+		int index = 0;
+		for (final Object obj : addThis) {
+			addStrings[index] = CastString(obj);
+			index++;
+		}
+		return MergeStrings(delim, addStrings);
+	}
+
+
+
+	/**
+	 * Generate a random string hash.
+	 * @param length Number of characters to generate
+	 * @return The generated hash string
+	 */
+	public static String RandomString(final int length) {
+		if (length < 1) return null;
+		final StringBuilder buf = new StringBuilder(length);
+		while (buf.length() < length) {
+			final String str = UUID.randomUUID().toString();
+			if (str == null) throw new RequiredArgumentException("str");
+			buf.append(str);
+		}
+		return
+			buf.toString()
+				.substring(
+					0,
+					NumberUtils.MinMax(length, 0, buf.length())
+				);
+	}
+
+
+
+	// generate regex from string with wildcard *
+	public static String WildcardToRegex(final String wildcard) {
+		final StringBuilder buf = new StringBuilder(wildcard.length());
+		buf.append('^');
+		final int len = wildcard.length();
+		for (int i = 0; i < len; i++) {
+			char c = wildcard.charAt(i);
+			switch (c) {
+			case '*':
+				buf.append(".*");
+				break;
+			case '?':
+				buf.append(".");
+				break;
+			case '(':
+			case ')':
+			case '[':
+			case ']':
+			case '$':
+			case '^':
+			case '.':
+			case '{':
+			case '}':
+			case '|':
+			case '\\':
+				buf.append('\\').append(c);
+				break;
+			default:
+				buf.append(c);
+				break;
+			}
+		}
+		buf.append('$');
+		return buf.toString();
+	}
+
+
+
+	// ------------------------------------------------------------------------------- //
 	// find position
 
 
@@ -579,6 +747,21 @@ if (caseInsensitive) throw new UnsupportedOperationException("UNFINISHED ARGUMEN
 			: pos
 		);
 	}
+
+
+	// find longest line
+	public static int FindLongestLine(final String[] lines) {
+		if (Utils.isEmpty(lines))
+			return -1;
+		int len = 0;
+		for (final String line : lines) {
+			if (line == null) continue;
+			if (line.length() > len)
+				len = line.length();
+		}
+		return len;
+	}
+
 
 
 	// ------------------------------------------------------------------------------- //
@@ -841,6 +1024,283 @@ if (caseInsensitive) throw new UnsupportedOperationException("UNFINISHED ARGUMEN
 	}
 	public static String PadCenter(final int width, final int value) {
 		return PadCenter( width, Integer.toString(value), '0' );
+	}
+
+
+
+	// ------------------------------------------------------------------------------- //
+	// replace {} tags
+
+
+
+	// replace {} or {#} tags
+	public static String ReplaceTags(final String msg, final Object... args) {
+		if (Utils.isEmpty(msg))  return msg;
+		if (Utils.isEmpty(args)) return msg;
+		final StringBuilder result = new StringBuilder(msg);
+		ARG_LOOP:
+		for (int index=0; index<args.length; index++) {
+			final Object obj = args[index];
+			final String str = (
+				obj == null
+				? "<null>"
+				: CastString(obj)
+			);
+			// {#}
+			{
+				final String tag =
+					(new StringBuilder())
+						.append('{')
+						.append(index + 1)
+						.append('}')
+						.toString();
+				boolean found = false;
+				REPLACE_LOOP:
+				while (true) {
+					final int pos = result.indexOf(tag);
+					if (pos == -1)
+						break REPLACE_LOOP;
+					result.replace(
+						pos,
+						pos + tag.length(),
+						str
+					);
+					found = true;
+				} // end REPLACE_LOOP
+				if (found)
+					continue ARG_LOOP;
+			}
+			// {}
+			{
+				final int pos = result.indexOf("{}");
+				if (pos >= 0) {
+					result.replace(
+						pos,
+						pos + 2,
+						str
+					);
+					continue ARG_LOOP;
+				}
+			}
+			// append
+			result
+				.append(' ')
+				.append(str);
+		} // end ARG_LOOP
+		return result.toString();
+	}
+
+
+
+	// replace {} or {#} tags (in multiple lines)
+	public static String[] ReplaceTags(final String[] msgs, final Object... args) {
+		if (Utils.isEmpty(msgs)) return msgs;
+		if (Utils.isEmpty(args)) return msgs;
+		String[] result = Arrays.copyOf(msgs, msgs.length);
+		final StringBuilder extras = new StringBuilder();
+		ARG_LOOP:
+		for (int argIndex=0; argIndex<args.length; argIndex++) {
+			final String str = (
+				args[argIndex] == null
+				? "<null>"
+				: CastString(args[argIndex])
+			);
+			// {#} - all instances
+			{
+				final String tag =
+					(new StringBuilder())
+						.append('{')
+						.append(argIndex + 1)
+						.append('}')
+						.toString();
+				boolean found = false;
+				LINE_LOOP:
+				for (int lineIndex=0; lineIndex<msgs.length; lineIndex++) {
+					if (Utils.isEmpty( result[lineIndex] ))
+						continue LINE_LOOP;
+					//REPLACE_LOOP:
+					while (true) {
+						final int pos = result[lineIndex].indexOf(tag);
+						if (pos == -1)
+							continue LINE_LOOP;
+						result[lineIndex] =
+							ReplaceStringRange(
+								result[lineIndex],
+								str,
+								pos,
+								pos + tag.length()
+							);
+						found = true;
+					} // end REPLACE_LOOP
+				} // end LINE_LOOP
+				if (found)
+					continue ARG_LOOP;
+			}
+			// {} - first found
+			{
+				LINE_LOOP:
+				for (int lineIndex=0; lineIndex<msgs.length; lineIndex++) {
+					if (Utils.isEmpty( result[lineIndex] ))
+						continue LINE_LOOP;
+					final int pos = result[lineIndex].indexOf("{}");
+					if (pos == -1)
+						continue LINE_LOOP;
+					result[lineIndex] =
+						ReplaceStringRange(
+							result[lineIndex],
+							str,
+							pos,
+							pos + 2
+						);
+					continue ARG_LOOP;
+				} // end LINE_LOOP
+			}
+			// append to end
+			{
+				if ( extras.length() != 0 )
+					extras.append(' ');
+				extras.append(str);
+			}
+		} // end ARG_LOOP
+		if ( extras.length() != 0 ) {
+			if (result.length == 1) {
+				result[0] =
+					(new StringBuilder())
+					.append(result[0])
+					.append(' ')
+					.append(extras)
+					.toString();
+				return result;
+			}
+			String[] newResult = new String[ result.length + 1 ];
+			newResult[result.length] = extras.toString();
+			return newResult;
+		}
+		return result;
+	}
+
+
+
+	// replace {key} tags
+	public static String ReplaceTagKeys(final String msg, final Map<String, Object> args) {
+		if (Utils.isEmpty(msg))  return msg;
+		if (Utils.isEmpty(args)) return msg;
+		final StringBuilder result = new StringBuilder(msg);
+		ARG_LOOP:
+		for (final String key : args.keySet()) {
+			final Object obj = args.get(key);
+			final String str = (
+				obj == null
+				? "<null>"
+				: CastString(obj)
+			);
+			// {key}
+			{
+				final String tag =
+					(new StringBuilder())
+						.append('{')
+						.append(key)
+						.append('}')
+						.toString();
+				boolean found = false;
+				REPLACE_LOOP:
+				while (true) {
+					final int pos = result.indexOf(tag);
+					if (pos == -1)
+						break REPLACE_LOOP;
+					result.replace(
+						pos,
+						pos + tag.length(),
+						str
+					);
+					found = true;
+				} // end REPLACE_LOOP
+				if (found)
+					continue ARG_LOOP;
+			}
+			// {}
+			{
+				final int pos = result.indexOf("{}");
+				if (pos != -1) {
+					result.replace(
+						pos,
+						pos + 2,
+						str
+					);
+				}
+			}
+			// don't append
+		} // end ARG_LOOP
+		return result.toString();
+	}
+
+
+
+	// replace {key} tags (in multiple lines)
+	public static String[] ReplaceTagKeys(final String[] msgs, final Map<String, Object> args) {
+		if (Utils.isEmpty(msgs)) return msgs;
+		if (Utils.isEmpty(args)) return msgs;
+		String[] result = Arrays.copyOf(msgs, msgs.length);
+		ARG_LOOP:
+		for (final String key : args.keySet()) {
+			final Object obj = args.get(key);
+			final String str = (
+				obj == null
+				? "<null>"
+				: CastString(obj)
+			);
+			// {key}
+			{
+				final String tag =
+					(new StringBuilder())
+						.append('{')
+						.append(key)
+						.append('}')
+						.toString();
+				boolean found = false;
+				LINE_LOOP:
+				for (int lineIndex=0; lineIndex<msgs.length; lineIndex++) {
+					if (Utils.isEmpty( result[lineIndex] ))
+						continue LINE_LOOP;
+					//REPLACE_LOOP:
+					while (true) {
+						final int pos = result[lineIndex].indexOf(tag);
+						if (pos == -1)
+							continue LINE_LOOP;
+						result[lineIndex] =
+							ReplaceStringRange(
+								result[lineIndex],
+								str,
+								pos,
+								pos + tag.length()
+							);
+						found = true;
+					} // end REPLACE_LOOP
+				} // end LINE_LOOP
+				if (found)
+					continue ARG_LOOP;
+			}
+			// {}
+			{
+				LINE_LOOP:
+				for (int lineIndex=0; lineIndex<msgs.length; lineIndex++) {
+					if (Utils.isEmpty( result[lineIndex] ))
+						continue LINE_LOOP;
+					final int pos = result[lineIndex].indexOf("{}");
+					if (pos != -1) {
+						result[lineIndex] =
+							ReplaceStringRange(
+								result[lineIndex],
+								str,
+								pos,
+								pos + 2
+							);
+						continue ARG_LOOP;
+					}
+				} // end LINE_LOOP
+			}
+		}
+		return result;
 	}
 
 
