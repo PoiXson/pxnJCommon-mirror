e15a03a5d7fc9a6dbdfcd4a47caa99351d7acb4b
lorenzop <lorenzo@poixson.com>
2020-05-27 00:00:37 -0400

    cleaned up RunnableMethod class

--- core/src/com/poixson/tools/abstractions/RunnableMethod.java
+++ core/src/com/poixson/tools/abstractions/RunnableMethod.java
@@ -1,12 +1,11 @@
 package com.poixson.tools.abstractions;
 
 import java.lang.reflect.Method;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
 import com.poixson.exceptions.RequiredArgumentException;
-import com.poixson.tools.byref.StringRef;
 import com.poixson.utils.ReflectUtils;
+import com.poixson.utils.StringUtils;
 import com.poixson.utils.Utils;
 
 
@@ -17,36 +16,33 @@ public class RunnableMethod<V> extends xRunnable {
 	public final Object[] args;
 
 	protected final AtomicReference<V> result = new AtomicReference<V>(null);
-	protected final AtomicBoolean done = new AtomicBoolean(false);
 
 
 
 	public RunnableMethod(final Object container,
 			final String methodName, final Object...args) {
-		this(
-			null,
-			container,
-			ReflectUtils.getMethodByName(container, methodName, args),
-			args
-		);
+		if (container == null)         throw new RequiredArgumentException("container");
+		if (Utils.isEmpty(methodName)) throw new RequiredArgumentException("method");
+		this.method = ReflectUtils.getMethodByName(container, methodName, args);
+		this.container = container;
+		this.args      = args;
 	}
 	public RunnableMethod(final Object container,
-			final Method methodName, final Object...args) {
-		this(
-			null,
-			container,
-			methodName,
-			args
-		);
+			final Method method, final Object...args) {
+		if (container == null) throw new RequiredArgumentException("container");
+		if (method == null)    throw new RequiredArgumentException("method");
+		this.container = container;
+		this.method    = method;
+		this.args      = args;
 	}
 	public RunnableMethod(final String taskName, final Object container,
 			final String methodName, final Object...args) {
-		this(
-			taskName,
-			container,
-			ReflectUtils.getMethodByName(container, methodName, args),
-			args
-		);
+		if (container == null) throw new RequiredArgumentException("container");
+		if (Utils.isEmpty(methodName)) throw new RequiredArgumentException("method");
+		this.method = ReflectUtils.getMethodByName(container, methodName, args);
+		this.container = container;
+		this.args      = args;
+		this.setTaskName(taskName);
 	}
 	public RunnableMethod(final String taskName, final Object container,
 			final Method method, final Object...args) {
@@ -55,38 +51,39 @@ public class RunnableMethod<V> extends xRunnable {
 		this.container = container;
 		this.method    = method;
 		this.args      = args;
-		this.setTaskName(
-			Utils.isEmpty(taskName)
-			? this.getFullName()
-			: taskName
+		this.setTaskName(taskName);
+	}
+
+
+
+	// ------------------------------------------------------------------------------- //
+	// run method
+
+
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public void run() {
+		this.runCount.getAndIncrement();
+		this.result.set(
+			(V) ReflectUtils.InvokeMethod(
+				this.container,
+				this.method,
+				this.args
+			)
 		);
 	}
 
 
 
-	// invoke stored method
-	@SuppressWarnings("unchecked")
-	@Override
-	public void run() {
-		try {
-			this.result.set(
-				(V)ReflectUtils.InvokeMethod(
-					this.container,
-					this.method,
-					this.args
-				)
-			);
-		} finally {
-			this.done.set(true);
-		}
-	}
+	// ------------------------------------------------------------------------------- //
 
 
 
 	public String getFullName() {
 		return
 			(new StringBuilder())
-				.append( StringRef.getNew( this.container.getClass().getName() ).peekLastPart('.') )
+				.append( StringUtils.PeekLastPart(this.container.getClass().getName(), '.'))
 				.append("->")
 				.append( this.method.getName())
 				.append("()")
@@ -96,12 +93,9 @@ public class RunnableMethod<V> extends xRunnable {
 
 
 	public V getResult() {
-		if ( ! this.done.get() )
+		if (this.notRun())
 			return null;
 		return this.result.get();
-	}
-	public boolean isDone() {
-		return this.done.get();
 	}
 
 
