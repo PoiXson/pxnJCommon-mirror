bf46d6249409c35059cedad862b74397c9b0c1bb
lorenzop <lorenzo@poixson.com>
2020-06-13 11:20:02 -0400

    cleaned up xLogPrintStream class and moved code to xLogOutputStream

---
+++ core/src/com/poixson/logger/xLogOutputStream.java
@@ -0,0 +1,126 @@
+package com.poixson.logger;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.concurrent.atomic.AtomicReference;
+
+import com.poixson.exceptions.RequiredArgumentException;
+import com.poixson.logger.records.xLogRecord_Msg;
+
+
+//not thread safe - variables not volatile, assuming only used in one thread
+public class xLogOutputStream extends OutputStream {
+
+	private final xLog   log;
+	private final xLevel level;
+
+	private AtomicReference<StringBuilder> buffer = new AtomicReference<StringBuilder>(new StringBuilder());
+
+
+
+	public xLogOutputStream() {
+		this(null, null);
+	}
+	public xLogOutputStream(final xLog outputLog) {
+		this(outputLog, null);
+	}
+	public xLogOutputStream(final xLevel printLevel) {
+		this(null, printLevel);
+	}
+	public xLogOutputStream(final xLog outputLog, final xLevel printLevel) {
+		super();
+		if (outputLog == null) throw new RequiredArgumentException("outputLog");
+		this.log   = outputLog;
+		this.level = printLevel;
+	}
+
+
+
+	@Override
+	public void write(final int b) throws IOException {
+		if (this.log == null)
+			return;
+		if (b == '\r')
+			return;
+		// flush buffer
+		if (b == '\n') {
+			this.dump();
+			return;
+		}
+		// append to buffer
+		this.buffer.get()
+			.append( (char)b );
+	}
+	@Override
+	public void write(final byte b[]) throws IOException {
+		write(b, 0, b.length);
+	}
+	@Override
+	public void write(final byte bytes[], final int off,
+			final int len) throws IOException {
+		if (bytes == null) throw new NullPointerException();
+		if (off < 0)                      throw new IndexOutOfBoundsException();
+		if (off > bytes.length)           throw new IndexOutOfBoundsException();
+		if (len < 0)                      throw new IndexOutOfBoundsException();
+		if ( (off + len) > bytes.length ) throw new IndexOutOfBoundsException();
+		if ( (off + len) < 0)             throw new IndexOutOfBoundsException();
+		if (this.log == null) return;
+		if (len == 0)         return;
+		for (int i=0; i<len; i++) {
+			final byte b = bytes[ off+i ];
+			if (b == '\r')
+				continue;
+			// flush buffer
+			if (b == '\n') {
+				this.dump();
+				continue;
+			}
+			// append to buffer
+			this.buffer.get()
+				.append( (char) b );
+		}
+	}
+
+
+
+	public void dump() {
+		this.log
+			.publish(
+				new xLogRecord_Msg(
+					this.level,
+					this.buffer.toString(),
+					new Object[0]
+				)
+			);
+		// reset buffer
+		{
+			final StringBuilder buf = this.buffer.get();
+			if (buf.length() > 40) {
+				this.buffer.set( new StringBuilder() );
+			} else {
+				buf.setLength(0);
+			}
+		}
+	}
+
+
+
+	@Override
+	public void flush() throws IOException {
+		this.log.flush();
+	}
+
+
+
+	// ------------------------------------------------------------------------------- //
+	// logger
+
+
+
+	public xLog log() {
+		return this.log;
+	}
+
+
+
+}
--- core/src/com/poixson/logger/xLogPrintStream.java
+++ core/src/com/poixson/logger/xLogPrintStream.java
@@ -1,7 +1,5 @@
 package com.poixson.logger;
 
-import java.io.IOException;
-import java.io.OutputStream;
 import java.io.PrintStream;
 
 
@@ -38,119 +36,20 @@ public class xLogPrintStream extends PrintStream {
 
 
 
-	public xLog getLog() {
-		return this.log;
-	}
-
-
-
 	@Override
 	public void flush() {
+		this.log.flush();
 	}
 
 
 
 	// ------------------------------------------------------------------------------- //
-	// output stream class
+	// logger
 
 
 
-	//not thread safe - variables not volatile, assuming only used in one thread
-	public static class xLogOutputStream extends OutputStream {
-
-		private final xLog   log;
-		private final xLevel level;
-
-		private StringBuilder buffer;
-
-
-
-		public xLogOutputStream() {
-			this(null, null);
-		}
-		public xLogOutputStream(final xLog outputLog) {
-			this(outputLog, null);
-		}
-		public xLogOutputStream(final xLevel printLevel) {
-			this(null, printLevel);
-		}
-		public xLogOutputStream(final xLog outputLog, final xLevel printLevel) {
-			super();
-			this.log   = outputLog;
-			this.level = printLevel;
-			this.buffer = (
-				outputLog == null
-				? null
-				: new StringBuilder()
-			);
-		}
-
-
-
-		@Override
-		public void write(final int b) throws IOException {
-			if (this.log == null)
-				return;
-			if (b == '\r')
-				return;
-			// flush buffer
-			if (b == '\n') {
-				this.flush();
-				return;
-			}
-			// append to buffer
-			this.buffer
-				.append( (char) b );
-		}
-		@Override
-		public void write(final byte b[]) throws IOException {
-			write(b, 0, b.length);
-		}
-		@Override
-		public void write(final byte bytes[], final int off,
-				final int len) throws IOException {
-			if (bytes == null) throw new NullPointerException();
-			if (off < 0)                      throw new IndexOutOfBoundsException();
-			if (off > bytes.length)           throw new IndexOutOfBoundsException();
-			if (len < 0)                      throw new IndexOutOfBoundsException();
-			if ( (off + len) > bytes.length ) throw new IndexOutOfBoundsException();
-			if ( (off + len) < 0)             throw new IndexOutOfBoundsException();
-			if (this.log == null) return;
-			if (len == 0)         return;
-			for (int i=0; i<len; i++) {
-				final byte b = bytes[ off+i ];
-				if (b == '\r')
-					continue;
-				// flush buffer
-				if (b == '\n') {
-					this.flush();
-					continue;
-				}
-				// append to buffer
-				this.buffer
-					.append( (char) b );
-			}
-		}
-
-
-
-		@Override
-		public void flush() throws IOException {
-			this.log
-				.publish(
-					this.level,
-					this.buffer.toString()
-				);
-			// reset buffer
-			if (this.buffer.length() > 40) {
-				this.buffer = new StringBuilder();
-			} else {
-				this.buffer.setLength(0);
-			}
-		}
-
-
-
+	public xLog log() {
+		return this.log;
 	}
 
 
