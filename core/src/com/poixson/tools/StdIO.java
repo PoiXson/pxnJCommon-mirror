cbeb5a11b9e2729403f3e61c6d5670d1d9ea49f2
lorenzop <lorenzo@poixson.com>
2020-06-13 11:55:39 -0400

    added new StdIO class

---
+++ core/src/com/poixson/tools/StdIO.java
@@ -0,0 +1,17 @@
+package com.poixson.tools;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+
+
+public final class StdIO {
+	private StdIO() {}
+	static { Keeper.add(new StdIO()); }
+
+
+	public static final PrintStream OriginalOut = System.out;
+	public static final PrintStream OriginalErr = System.err;
+	public static final InputStream OriginalIn  = System.in;
+
+
+}
