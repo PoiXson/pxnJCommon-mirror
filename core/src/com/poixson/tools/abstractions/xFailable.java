1ecaec929fed29a6d0bd5f41eca914eaccfd80a0
lorenzop <lorenzo@poixson.com>
2020-07-22 20:09:06 -0400

    added fail(Throwable) to xFailable interface

--- core/src/com/poixson/tools/abstractions/xFailable.java
+++ core/src/com/poixson/tools/abstractions/xFailable.java
@@ -4,8 +4,9 @@ package com.poixson.tools.abstractions;
 public interface xFailable {
 
 
-	public void fail(final int exitCode, final String msg, final Object...args);
+	public void fail(final Throwable e);
 	public void fail(final String msg, final Object...args);
+	public void fail(final int exitCode, final String msg, final Object...args);
 
 	public boolean hasFailed();
 
