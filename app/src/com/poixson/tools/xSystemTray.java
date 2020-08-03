c0e45b1c9600917ea8ab4a0762357801ca982f41
lorenzop <lorenzo@poixson.com>
2020-08-03 08:56:14 -0400

    added xSystemTray class

---
+++ app/src/com/poixson/tools/xSystemTray.java
@@ -0,0 +1,55 @@
+package com.poixson.tools;
+
+import static com.poixson.logger.xLog.XLog;
+
+import java.awt.AWTException;
+import java.awt.Image;
+import java.awt.PopupMenu;
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
+
+
+public abstract class xSystemTray {
+
+	protected final SystemTray tray;
+	protected final TrayIcon trayIcon;
+
+
+
+	public xSystemTray() {
+		if (!SystemTray.isSupported()) {
+			this.tray = null;
+			this.trayIcon = null;
+			return;
+		}
+		this.tray = SystemTray.getSystemTray();
+		// icon
+		final Image icon = this.loadIcon();
+		this.trayIcon = new TrayIcon(icon);
+		try {
+			this.tray.add(this.trayIcon);
+		} catch (AWTException e) {
+			throw new RuntimeException(e);
+		}
+		XLog().fine("Loaded system tray icon");
+	}
+
+
+
+	public void setMenu(final PopupMenu menu) {
+		this.trayIcon.setPopupMenu(menu);
+	}
+
+
+
+	public void remove() {
+		this.tray.remove(this.trayIcon);
+	}
+
+
+
+	protected abstract Image loadIcon();
+
+
+
+}
