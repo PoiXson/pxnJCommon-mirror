63dc55713aec14abf2bdd76a7197260539985937
lorenzop <lorenzo@poixson.com>
2020-08-04 13:52:34 -0400

    cleaned up and improved xFont class

--- app/src/com/poixson/tools/xFont.java
+++ app/src/com/poixson/tools/xFont.java
@@ -1,66 +1,43 @@
 package com.poixson.tools;
 
 import java.awt.Font;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
 
-import com.poixson.logger.xLog;
 import com.poixson.utils.NumberUtils;
 import com.poixson.utils.Utils;
-import com.poixson.utils.guiUtils;
 
 
 public class xFont {
 
-	public final AtomicReference<String> family = new AtomicReference<String>(null);
-	public final AtomicReference<Style>  style  = new AtomicReference<Style>(Style.PLAIN);
-	public final AtomicInteger size = new AtomicInteger(0);
-	public final int base = 12;
+	public String family = null;
+	public int style = Font.PLAIN;
+	public int size  = xFontDefault.Get().size;
 
 
 
-	public enum Style {
-		PLAIN (Font.PLAIN),
-		BOLD  (Font.BOLD),
-		ITALIC(Font.ITALIC);
-		public final int value;
-		Style(final int value) {
-			this.value = value;
-		}
+	public static xFont Build(final String format) {
+		return new xFont(format);
+	}
+	public static xFont Build(final xFont clone) {
+		return new xFont(clone);
+	}
+	public static xFont Build() {
+		return new xFont();
+	}
+
+	public static Font Font(final String format) {
+		return Build(format)
+				.font();
 	}
 
 
 
 	public xFont(final String format) {
-		this();
-		if (Utils.notEmpty(format)) {
-			this.apply(format);
-		}
+		this.apply(format);
 	}
-	public xFont(final xFont font) {
-		this();
-		this.family.set(font.family.get());
-		this.style.set( font.style.get() );
-		this.size.set(  font.size.get()  );
+	public xFont(final xFont clone) {
+		this.apply(clone);
 	}
 	public xFont() {
-	}
-
-
-
-	public Font font(final String format) {
-		if (Utils.isEmpty(format))
-			return this.font();
-		final xFont font = new xFont(this);
-		font.apply(format);
-		return font.font();
-	}
-	public Font font() {
-		return new Font(
-			this.family.get(),
-			this.style.get().value,
-			this.base + this.size.get()
-		);
 	}
 
 
@@ -68,66 +45,133 @@ public class xFont {
 	public xFont apply(final String format) {
 		if (Utils.isEmpty(format))
 			return this;
-		final StringParts parts = StringParts.getNew(format);
-		parts.delim(",");
-		while (parts.hasNext()) {
-			String part = parts.part();
-			if (part == null) break;
+		final String[] parts = format.split(",");
+		PARTS_LOOP:
+		for (String part : parts) {
 			part = part.trim().toLowerCase();
-			if (part.isEmpty()) continue;
-			// font style
+			if (part.isEmpty()) continue PARTS_LOOP;
+			// size
+			if (part.startsWith("size")) {
+				part = part.substring(4).trim();
+				if (part.startsWith("=") || part.startsWith(":")) {
+					part = part.substring(1).trim();
+				}
+				if (part.startsWith("+")) {
+					part = part.substring(1).trim();
+					final Integer size = NumberUtils.ToInteger(part);
+					if (size == null) throw new RuntimeException("Invalid font size: "+part);
+					final int sizeDef = xFontDefault.Get().size;
+					this.size(sizeDef + size.intValue());
+				} else {
+					final Integer size = NumberUtils.ToInteger(part);
+					if (size == null) throw new RuntimeException("Invalid font size: "+part);
+					this.size(size.intValue());
+				}
+				continue PARTS_LOOP;
+			}
+			// family
+			if (part.startsWith("fam")) {
+				part = (
+					part.startsWith("family")
+					? part.substring(6)
+					: part.substring(3)
+				);
+				if (part.startsWith("=") || part.startsWith(":")) {
+					part = part.substring(1).trim();
+				}
+				this.family(part);
+				continue PARTS_LOOP;
+			}
+			// style
 			switch (part) {
 			case "b":
 			case "bold":
-				this.style.set(Style.BOLD);
-				continue;
+				this.bold();
+				continue PARTS_LOOP;
 			case "i":
 			case "italic":
-				this.style.set(Style.ITALIC);
-				continue;
+				this.italic();
+				continue PARTS_LOOP;
 			case "p":
 			case "plain":
-				this.style.set(Style.PLAIN);
-				continue;
+				this.plain();
+				continue PARTS_LOOP;
 			default:
 			}
-			// font family
-			if (part.startsWith("fam")) {
-				String str = ( part.startsWith("family") ? part.substring(6) : part.substring(3) ).trim();
-				if (str.startsWith("=") || str.startsWith(":")) {
-					str = str.substring(1).trim();
-				}
-				this.family.set(str);
-				continue;
-			}
-			// font size
-			if (part.startsWith("size")) {
-				String str = part.substring(4).trim();
-				if (str.startsWith("=") || str.startsWith(":")) {
-					str = str.substring(1).trim();
-				}
-				if (str.startsWith("+")) {
-					str = str.substring(1).trim();
-				}
-				final Integer i = NumberUtils.ToInteger(str);
-				if (i == null) {
-					this.log().warning("Invalid font size value:", part);
-					continue;
-				}
-				this.size.set(i.intValue());
-				continue;
-			}
 			// unknown format
-			this.log().warning("Unknown font formatting:", part);
-		}
+			throw new RuntimeException("Unknown font format: "+part);
+		} // end PARTS_LOOP
+		return this;
+	}
+	public xFont apply(final xFont clone) {
+		this.family = clone.family;
+		this.style  = clone.style;
+		this.size   = clone.size;
 		return this;
 	}
 
 
 
-	// logger
-	public xLog log() {
-		return guiUtils.log();
+	// ------------------------------------------------------------------------------- //
+	// size
+
+
+
+	public xFont size(final int size) {
+		this.size = size;
+		return this;
+	}
+	public xFont sizePlus(final int add) {
+		this.size += add;
+		return this;
+	}
+
+
+
+	// ------------------------------------------------------------------------------- //
+	// style
+
+
+
+	public xFont plain() {
+		this.style = Font.PLAIN;
+		return this;
+	}
+	public xFont bold() {
+		this.style |= Font.BOLD;
+		return this;
+	}
+	public xFont italic() {
+		this.style |= Font.ITALIC;
+		return this;
+	}
+
+
+
+	// ------------------------------------------------------------------------------- //
+	// font family
+
+
+
+	public xFont family(final String family) {
+		this.family = family;
+		return this;
+	}
+
+
+
+	// ------------------------------------------------------------------------------- //
+
+
+
+	// to java font
+	public Font font() {
+		return
+			new Font(
+				this.family,
+				this.style,
+				this.size
+			);
 	}
 
 
