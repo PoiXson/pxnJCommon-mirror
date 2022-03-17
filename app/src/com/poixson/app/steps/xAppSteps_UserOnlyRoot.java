36c16c2e1e5c5c38a8f11514be0fdc5257a80ce8
lorenzop <lorenzo@poixson.com>
2022-03-16 20:55:58 -0400

    cleaned up and finished xAppSteps_UserNotRoot and added _UserOnlyRoot classes

--- app/src/com/poixson/app/steps/xAppSteps_UserOnlyRoot.java
+++ app/src/com/poixson/app/steps/xAppSteps_UserOnlyRoot.java
@@ -9,7 +9,7 @@ import com.poixson.utils.Utils;
  * Startup sequence
  *   10  check user
  */
-public interface xAppSteps_UserNotRoot {
+public interface xAppSteps_UserOnlyRoot {
 
 
 
@@ -18,9 +18,9 @@ public interface xAppSteps_UserNotRoot {
 
 
 
-	// ensure not root
+	// ensure is root
 	@xAppStep(mode=xAppStepMode.STARTUP, weight=10, title="Check User")
-	default void __START_user_not_root() {
+	default void __START_user_only_root() {
 		final String user = System.getProperty("user.name");
 		if (Utils.isEmpty(user))
 			return;
@@ -28,8 +28,9 @@ public interface xAppSteps_UserNotRoot {
 		case "root":
 		case "admin":
 		case "administrator":
-			throw new RuntimeException("Cannot run this program as root/administrator");
-		default: break;
+			break;
+		default:
+			throw new RuntimeException("This program can only run as root/administrator");
 		}
 	}
 
