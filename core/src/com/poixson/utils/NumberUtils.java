423d7460c60c7ca946da37afa417bfea582494fb
lorenzop <lorenzo@poixson.com>
2023-01-16 16:16:32 -0500

    fixed NumberUtils::GetRandom() argument checks

--- core/src/com/poixson/utils/NumberUtils.java
+++ core/src/com/poixson/utils/NumberUtils.java
@@ -561,6 +561,8 @@ public final class NumberUtils {
 		return GetRandom(minNumber, maxNumber, ((long)seed));
 	}
 	public static int GetRandom(final int minNumber, final int maxNumber, final long seed) {
+		if (minNumber >  maxNumber) return minNumber;
+		if (minNumber == maxNumber) return minNumber;
 		if (seed == 0L)
 			return GetRandom(minNumber, maxNumber, 1L);
 		final Random gen = new Random(Utils.GetMS() * seed);
@@ -568,8 +570,8 @@ public final class NumberUtils {
 	}
 
 	public static int GetNewRandom(final int minNumber, final int maxNumber, final int oldNumber) {
-		if (minNumber == maxNumber)
-			return minNumber;
+		if (minNumber >  maxNumber) return minNumber;
+		if (minNumber == maxNumber) return minNumber;
 		if ((maxNumber - minNumber) == 1)
 			return (oldNumber == minNumber ? maxNumber : minNumber);
 		int newNumber;
