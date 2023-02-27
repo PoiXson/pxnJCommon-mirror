d01b53e1328816b5b29f9fe30a060ff7ac538e6b
lorenzop <lorenzo@poixson.com>
2023-02-27 10:03:07 -0500

    added new InvalidValueException

---
+++ core/src/com/poixson/exceptions/InvalidValueException.java
@@ -0,0 +1,19 @@
+package com.poixson.exceptions;
+
+import com.poixson.utils.StringUtils;
+
+public class InvalidValueException extends IllegalArgumentException {
+	private static final long serialVersionUID = 1L;
+
+
+
+	public InvalidValueException(final String name) {
+		super("Invalid value: " + name);
+	}
+	public InvalidValueException(final String name, final Object value) {
+		super("Invalid value: " + name + " = " + StringUtils.ToString(value));
+	}
+
+
+
+}
