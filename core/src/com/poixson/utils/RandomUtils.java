1b9ea49f4fceaa3f66a9c2b15bcf4dfd5baee34e
lorenzop <lorenzo@poixson.com>
2023-05-21 16:20:05 -0400

    removed old Rnd10K() function and reused LastRND variable

--- core/src/com/poixson/utils/RandomUtils.java
+++ core/src/com/poixson/utils/RandomUtils.java
@@ -10,13 +10,12 @@ public final class RandomUtils {
 	private RandomUtils() {}
 	static { Keeper.add(new RandomUtils()); }
 
-	protected static final AtomicInteger Last10K = new AtomicInteger(0);
+	protected static final AtomicInteger LastRND = new AtomicInteger(0);
 
 
 
-//TODO: does this replace HistoryRND class?
 	public static int GetRandom(final int minNumber, final int maxNumber) {
-		return GetRandom(minNumber, maxNumber, 1L);
+		return GetRandom(minNumber, maxNumber, LastRND.get());
 	}
 	public static int GetRandom(final int minNumber, final int maxNumber, final int seed) {
 		return GetRandom(minNumber, maxNumber, ((long)seed));
@@ -26,8 +25,10 @@ public final class RandomUtils {
 		if (minNumber == maxNumber) return minNumber;
 		if (seed == 0L)
 			return GetRandom(minNumber, maxNumber, 1L);
-		final Random gen = new Random(Utils.GetMS() * seed);
-		return gen.nextInt(maxNumber - minNumber) + minNumber;
+		final Random rnd = new Random(Utils.GetMS() * seed);
+		final int num = rnd.nextInt(maxNumber - minNumber) + minNumber;
+		LastRND.set(num * LastRND.get());
+		return num;
 	}
 
 	public static int GetNewRandom(final int minNumber, final int maxNumber, final int oldNumber) {
@@ -39,20 +40,13 @@ public final class RandomUtils {
 		int seed = oldNumber;
 		for (int i=0; i<100; i++) {
 			newNumber = GetRandom(minNumber, maxNumber, seed);
-			if (newNumber != oldNumber)
+			if (newNumber != oldNumber) {
+				LastRND.set(newNumber * LastRND.get());
 				return newNumber;
+			}
 			seed += i;
 		}
 		throw new IllegalAccessError("Failed to generate a random number");
-	}
-
-
-
-	@Deprecated
-	public static int Rnd10K() {
-		final int rnd = GetNewRandom(0, 9999, Last10K.get());
-		Last10K.set(rnd);
-		return rnd;
 	}
 
 
