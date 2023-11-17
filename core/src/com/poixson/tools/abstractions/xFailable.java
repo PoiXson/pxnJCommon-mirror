2b3291f677a287e59795c89ca253788bbb2a46b5
lorenzop <lorenzo@poixson.com>
2023-11-16 19:18:13 -0500

    changed xFailable->fail() to return a boolean and added onFailure() listener

--- core/src/com/poixson/tools/abstractions/xFailable.java
+++ core/src/com/poixson/tools/abstractions/xFailable.java
@@ -4,11 +4,13 @@ package com.poixson.tools.abstractions;
 public interface xFailable {
 
 
-	public void fail(final Throwable e);
-	public void fail(final String msg, final Object...args);
-	public void fail(final int exitCode, final String msg, final Object...args);
+	public boolean fail(final Throwable e);
+	public boolean fail(final String msg, final Object...args);
+	public boolean fail(final int exitCode, final String msg, final Object...args);
 
 	public boolean hasFailed();
+
+	public void onFailure();
 
 
 }
