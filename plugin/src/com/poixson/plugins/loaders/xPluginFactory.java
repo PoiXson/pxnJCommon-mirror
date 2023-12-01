465d24d6083dc8b7d3e957d418d1324c0a7c384b
lorenzop <lorenzo@poixson.com>
2023-12-01 01:23:38 -0500

    moved try/catch from xPluginFactory->build() function to getConstruct() and getPlugin() protected functions

--- plugin/src/com/poixson/plugins/loaders/xPluginFactory.java
+++ plugin/src/com/poixson/plugins/loaders/xPluginFactory.java
@@ -20,23 +20,10 @@ public class xPluginFactory<T extends xJavaPlugin> {
 
 	public T build(final xPluginManager<T> manager, final xPluginYML yml,
 			final Class<T> clss, final String class_main) throws IOException {
-		final Constructor<T> construct;
-		try {
-			construct = this.getConstruct(clss);
-		} catch (NoSuchMethodException e) {
-			throw new IOException(e);
-		} catch (SecurityException e) {
-			throw new IOException(e);
-		}
+		final Constructor<T> construct = this.getConstruct(clss);
 		if (construct == null)
 			throw new IOException("Failed to get instance constructor: " + class_main);
-		final T plugin;
-		try {
-			plugin = construct.newInstance(manager, yml);
-		} catch (InstantiationException    e) { throw new IOException(e);
-		} catch (IllegalAccessException    e) { throw new IOException(e);
-		} catch (IllegalArgumentException  e) { throw new IOException(e);
-		} catch (InvocationTargetException e) { throw new IOException(e); }
+		final T plugin = this.getPlugin(construct, manager, yml);
 		if (plugin == null)
 			throw new IOException("Failed to create new instance of plugin class: " + class_main);
 		return plugin;
@@ -45,15 +32,22 @@ public class xPluginFactory<T extends xJavaPlugin> {
 
 
 	protected Constructor<T> getConstruct(final Class<T> clss)
-			throws NoSuchMethodException, SecurityException {
-		return clss.getConstructor(xPluginManager.class, xPluginYML.class);
+			throws IOException {
+		try {
+			return clss.getConstructor(xPluginManager.class, xPluginYML.class);
+		} catch (NoSuchMethodException e) { throw new IOException(e);
+		} catch (SecurityException     e) { throw new IOException(e); }
 	}
 
 	protected T getPlugin(final Constructor<T> construct,
 			final xPluginManager<T> manager, final xPluginYML yml)
-			throws InstantiationException, IllegalAccessException,
-			IllegalArgumentException, InvocationTargetException {
-		return construct.newInstance(manager, yml);
+			throws IOException {
+		try {
+			return construct.newInstance(manager, yml);
+		} catch (InstantiationException    e) { throw new IOException(e);
+		} catch (IllegalAccessException    e) { throw new IOException(e);
+		} catch (IllegalArgumentException  e) { throw new IOException(e);
+		} catch (InvocationTargetException e) { throw new IOException(e); }
 	}
 
 
