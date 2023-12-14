c9869d34285eed30b3f14d56d53feaf310a77229
lorenzop <lorenzo@poixson.com>
2023-12-14 18:32:16 -0500

    cleaned up OutputStreamLineRemapper class

--- core/src/com/poixson/tools/abstractions/OutputStreamLineRemapper.java
+++ core/src/com/poixson/tools/abstractions/OutputStreamLineRemapper.java
@@ -7,37 +7,45 @@ import java.io.PrintStream;
 
 public abstract class OutputStreamLineRemapper extends OutputStream {
 
-	private final StringBuilder buf = new StringBuilder();
+	private final StringBuilder buffer = new StringBuilder();
 
 
 
 	public static PrintStream toPrintStream(final OutputStream outstream) {
 		return new PrintStream(outstream, false);
 	}
+
 	public OutputStreamLineRemapper() {
 		super();
 	}
 
 
 
-	public abstract void line(final String line);
+	public abstract void print(final String line);
 
 
 
 	@Override
-	public void write(final int b) throws IOException {
-		if (b == '\r')
-			return;
-		if (b == '\n') {
-			this.line(
-				this.buf.toString()
-			);
-			this.buf.setLength(0);
-			return;
-		}
-		this.buf.append(
-			Character.toChars(b)
-		);
+	public void write(final int chr) throws IOException {
+		if (chr == '\r') return;
+		if (chr == '\n') this.flush();
+		else this.buffer.append( (char)chr );
+	}
+
+
+
+	@Override
+	public void flush() {
+		final String line = this.buffer.toString();
+		this.buffer.setLength(0);
+		this.print(line);
+	}
+
+
+
+	@Override
+	public void close() {
+		this.flush();
 	}
 
 
