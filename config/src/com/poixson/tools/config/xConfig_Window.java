30559863105df27afb2ad786ba637145210f82a3
lorenzop <lorenzo@poixson.com>
2023-12-14 20:20:33 -0500

    added xConfig_Window class

---
+++ config/src/com/poixson/tools/config/xConfig_Window.java
@@ -0,0 +1,30 @@
+package com.poixson.tools.config;
+
+import java.util.Map;
+
+
+public class xConfig_Window extends xConfig {
+
+	public final int x;
+	public final int y;
+	public final int w;
+	public final int h;
+
+
+
+	public xConfig_Window(final Map<String, Object> datamap) {
+		this(datamap, -1, -1, -1, -1);
+	}
+	public xConfig_Window(final Map<String, Object> datamap,
+			final int def_x, final int def_y,
+			final int def_w, final int def_h) {
+		super(datamap);
+		this.x = this.getInt("x", def_x);
+		this.y = this.getInt("y", def_y);
+		this.w = this.getInt("w", def_w);
+		this.h = this.getInt("h", def_h);
+	}
+
+
+
+}
