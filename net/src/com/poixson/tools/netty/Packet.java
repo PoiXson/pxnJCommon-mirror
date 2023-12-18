79bc978198a5d865d9b0f526e5f2a5a8a025fbf4
lorenzop <lorenzo@poixson.com>
2023-12-17 21:08:41 -0500

    added Packet interface and PacketException class

---
+++ net/src/com/poixson/tools/netty/Packet.java
@@ -0,0 +1,7 @@
+package com.poixson.tools.netty;
+
+
+public interface Packet {
+
+
+}
---
+++ net/src/com/poixson/tools/netty/exceptions/PacketException.java
@@ -0,0 +1,15 @@
+package com.poixson.tools.netty.exceptions;
+
+
+public class PacketException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+
+
+	public PacketException(final String msg) {
+		super(msg);
+	}
+
+
+
+}
