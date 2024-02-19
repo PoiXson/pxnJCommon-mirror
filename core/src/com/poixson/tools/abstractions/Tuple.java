ec4240d58c586b0fa353e2367789ece9f8260e25
lorenzop <lorenzo@poixson.com>
2024-02-18 21:34:32 -0500

    simplified Tuple->equals() function by using hashCode()

--- core/src/com/poixson/tools/abstractions/Tuple.java
+++ core/src/com/poixson/tools/abstractions/Tuple.java
@@ -15,24 +15,14 @@ public class Tuple<K, V> {
 
 
 
+//TODO: fix this and unit test
 	@Override
 	public boolean equals(final Object match) {
 		if (this == match)             return true;
 		if (!(match instanceof Tuple)) return false;
 		@SuppressWarnings("unchecked")
 		final Tuple<K, V> tup = (Tuple<K, V>) match;
-		int i = 0;
-		if (this.key == null && tup.key == null) i++;
-		if (this.val == null && tup.val == null) i++;
-		if (i == 2) return true;
-		if (i == 1) {
-			if (this.key == null) return this.val.equals(tup.val);
-			else                  return this.key.equals(tup.key);
-		}
-		return (
-			this.key.equals(tup.key) &&
-			this.val.equals(tup.val)
-		);
+		return (this.hashCode() == tup.hashCode());
 	}
 
 
@@ -40,12 +30,9 @@ public class Tuple<K, V> {
 	@Override
 	public String toString() {
 		return (new StringBuilder())
-			.append('[')
-			.append(this.key)
-			.append(',')
-			.append(this.val)
-			.append(']')
-			.toString();
+			.append('[').append(this.key)
+			.append(',').append(this.val)
+			.append(']').toString();
 	}
 
 	@Override
