60ad1e39f4840ac7fcc7850b9bb1469d9e586fac
lorenzop <lorenzo@poixson.com>
2024-08-20 02:14:46 -0400

    simplified xFailable->fail() by removing optional args

--- core/src/com/poixson/tools/abstractions/xFailable.java
+++ core/src/com/poixson/tools/abstractions/xFailable.java
@@ -5,7 +5,7 @@ public interface xFailable {
 
 
 	public boolean fail(final Throwable e);
-	public boolean fail(final String msg, final Object...args);
+	public boolean fail(final String msg);
 
 	public boolean isFailed();
 
--- core/src/com/poixson/tools/abstractions/xFailableApp.java
+++ core/src/com/poixson/tools/abstractions/xFailableApp.java
@@ -4,7 +4,7 @@ package com.poixson.tools.abstractions;
 public interface xFailableApp extends xFailable {
 
 
-	public boolean fail(final int exitCode, final String msg, final Object...args);
+	public boolean fail(final int exitCode, final String msg);
 	public int getExitCode();
 
 
