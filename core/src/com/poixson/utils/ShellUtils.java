51b754a19d15ed10f639600ce3fddc5bb3b079f8
lorenzop <lorenzo@poixson.com>
2024-08-20 02:17:30 -0400

    simplified ShellUtils class by merging in ShellUtils_Extended

--- core/src/com/poixson/utils/ShellUtils.java
+++ core/src/com/poixson/utils/ShellUtils.java
@@ -2,14 +2,22 @@ package com.poixson.utils;
 
 import static com.poixson.utils.Utils.IsEmpty;
 
-import java.util.concurrent.atomic.AtomicReference;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.fusesource.jansi.Ansi;
 
 import com.poixson.tools.Keeper;
 
 
-public class ShellUtils {
+public final class ShellUtils {
+	private ShellUtils() {}
+	static { Keeper.add(new ShellUtils()); }
 
-	protected static final AtomicReference<ShellUtils> instance = new AtomicReference<ShellUtils>(null);
+
+
+	// -------------------------------------------------------------------------------
+	// colors
 
 
 
@@ -26,50 +34,26 @@ public class ShellUtils {
 
 
 
-	protected static ShellUtils Get() {
-		if (instance.get() == null) {
-			// extended utility
-			final Class<ShellUtils> clss =
-				ReflectUtils.GetClass(
-					"com.poixson.utils.ShellUtils_Extended"
-				);
-			if (clss != null) {
-				final ShellUtils utility = (ShellUtils) ReflectUtils.NewInstance(clss);
-				if (utility == null) throw new RuntimeException("Unable to initialize ShellUtils instance");
-				if (instance.compareAndSet(null, utility))
-					return utility;
-				return instance.get();
-			}
-			// default utility
-			final ShellUtils utility = new ShellUtils();
-			if (instance.compareAndSet(null, utility))
-				return utility;
-		}
-		return instance.get();
-	}
-	protected ShellUtils() {
-		Keeper.add(this);
-	}
-
-
-
-	// -------------------------------------------------------------------------------
-	// colors
-
-
-
 	public static String RenderAnsi(final String line) {
-		return Get()._renderAnsi(line);
-	}
-	protected String _renderAnsi(final String line) {
-		return StripColorTags(line);
+//TODO: if not color
+//return StripColorTags(line);
+//TODO: AnsiConsole.systemInstall();
+		if (IsEmpty(line))
+			return line;
+		return Ansi.ansi()
+			.render(line)
+			.toString();
 	}
 
 	public static String[] RenderAnsi(final String[] lines) {
-		return Get()._renderAnsi(lines);
-	}
-	protected String[] _renderAnsi(final String[] lines) {
-		return StripColorTags(lines);
+//TODO: if not color
+//return StripColorTags(lines);
+		if (IsEmpty(lines))
+			return lines;
+		String[] result = new String[ lines.length ];
+		for (int index=0; index<lines.length; index++)
+			result[index] = (IsEmpty(lines[index]) ? "" : Ansi.ansi().render( lines[index] ).toString());
+		return result;
 	}
 
 
