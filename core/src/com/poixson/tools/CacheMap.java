9b7299974a9aa0f2ed76e336bf84b6ce383c917e
lorenzop <lorenzo@poixson.com>
2024-10-30 03:04:25 -0400

    rewrote CacheMap class to be extendable and splitting out new CacheMapSoft class

--- core/src/com/poixson/tools/CacheMap.java
+++ core/src/com/poixson/tools/CacheMap.java
@@ -1,59 +1,166 @@
 package com.poixson.tools;
 
-import java.lang.ref.SoftReference;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.poixson.tools.abstractions.Triple;
 
 
 public class CacheMap<K, V> implements Map<K, V> {
 
-	protected static final int DEFAULT_CLEANUP_TICKS  = 10;
-	protected static final int DEFAULT_CLEANUP_CYCLES = 20;
+	public static final String SECONDS_PER_CYCLE = "10s";
+	protected static final long DEFAULT_CYCLES_TIMEOUT  = (int) xTime.Parse( "2m").get(SECONDS_PER_CYCLE);
+	protected static final long DEFAULT_CYCLES_SAVE     = (int) xTime.Parse("20s").get(SECONDS_PER_CYCLE);
+	protected static final long DEFAULT_CYCLES_SAVE_MAX = (int) xTime.Parse( "1m").get(SECONDS_PER_CYCLE);
 
-	protected final ConcurrentHashMap<K, SoftReference<V>> map = new ConcurrentHashMap<K, SoftReference<V>>();
-	protected final ConcurrentHashMap<K, AtomicInteger> map_cycles = new ConcurrentHashMap<K, AtomicInteger>();
+	protected final ConcurrentHashMap<K, V> map = new ConcurrentHashMap<K, V>();
+	// <accessed, changed-first, changed-last>
+	protected final ConcurrentHashMap<K, Triple<AtomicLong, AtomicLong, AtomicLong>> cycles =
+			new ConcurrentHashMap<K, Triple<AtomicLong, AtomicLong, AtomicLong>>();
 
-	protected final int cleanup_ticks;
-	protected final int cleanup_cycles;
-	protected final AtomicInteger ticks = new AtomicInteger(0);
+	// timeout and save = ticks x cycles
+	protected final long cycles_timeout;
+	protected final long cycles_save;
+	protected final long cycles_save_max;
+	protected final AtomicLong ticks = new AtomicLong(0);
 
 
 
 	public CacheMap() {
 		this(
-			DEFAULT_CLEANUP_TICKS,
-			DEFAULT_CLEANUP_CYCLES
+			DEFAULT_CYCLES_TIMEOUT,
+			DEFAULT_CYCLES_SAVE,
+			DEFAULT_CYCLES_SAVE_MAX
 		);
 	}
-	public CacheMap(final int cleanup_ticks, final int cleanup_cycles) {
-		this.cleanup_ticks  = cleanup_ticks;
-		this.cleanup_cycles = cleanup_cycles;
+	public CacheMap(final long cycles_timeout,
+			final long cycles_save, final long cycles_save_max) {
+		this.cycles_timeout  = cycles_timeout;
+		this.cycles_save     = cycles_save;
+		this.cycles_save_max = cycles_save_max;
 	}
 
 
 
-	public void cleanup() {
-		if (this.ticks.incrementAndGet() >= this.cleanup_ticks) {
-			this.ticks.set(0);
-			final LinkedList<K> remove = new LinkedList<K>();
-			final Iterator<Entry<K, AtomicInteger>> it = this.map_cycles.entrySet().iterator();
-			while (it.hasNext()) {
-				final Entry<K, AtomicInteger> entry = it.next();
-				final int cycles = entry.getValue().incrementAndGet();
-				if (cycles >= this.cleanup_cycles)
-					remove.add(entry.getKey());
-			}
-			for (final K key : remove) {
-				this.map.remove(key);
-				this.map_cycles.remove(key);
+	public V load(final K key) {
+		return null;
+	}
+
+	public void save(final K key) {
+		this.mark_saved(key);
+	}
+
+	public int saveAll() {
+		return this.saveAll(false);
+	}
+	public int saveAllInvalidate() {
+		return this.saveAll(true);
+	}
+	protected int saveAll(final boolean invalidate) {
+		int count = 0;
+		for (final K key : this.map.keySet()) {
+			this.save(key);
+			if (invalidate)
+				this.invalidate(key);
+			count++;
+		}
+		return count;
+	}
+
+
+
+	public void tick() {
+		// check timeout/save cycles
+		{
+			final LinkedList<K> keys = new LinkedList<K>();
+			for (final K key : this.map.keySet())
+				keys.addLast(key);
+			for (final K key : keys)
+				this.tick(key);
+		}
+		// cleanup removed
+		{
+			final LinkedList<K> keys = new LinkedList<K>();
+			for (final K key : this.cycles.keySet())
+				keys.addLast(key);
+			for (final K key : keys) {
+				if (!this.map.containsKey(key))
+					this.cycles.remove(key);
 			}
 		}
+	}
+	protected void tick(final K key) {
+		final Triple<AtomicLong, AtomicLong, AtomicLong> cycles = this.getCycles(key);
+		final long cycle_accessed      = cycles.key.incrementAndGet();
+		final long cycle_changed_first = cycles.val.incrementAndGet();
+		final long cycle_changed_last  = cycles.ent.incrementAndGet();
+		if (cycle_accessed      < 0L) cycles.key.set(Long.MIN_VALUE);
+		if (cycle_changed_first < 0L) cycles.val.set(Long.MIN_VALUE);
+		if (cycle_changed_last  < 0L) cycles.ent.set(Long.MIN_VALUE);
+		// save
+		if (cycle_changed_first >= this.cycles_save_max
+		||  cycle_changed_last  >= this.cycles_save) {
+			this.save(key);
+		} else
+		// expire
+		if (cycle_accessed >= this.cycles_timeout) {
+			if (cycle_changed_first >= 0L
+			||  cycle_changed_last  >= 0L)
+				this.save(key);
+			this.invalidate(key);
+		}
+	}
+
+	public void invalidate(final K key) {
+		this.map.remove(key);
+	}
+
+
+
+	public void mark_accessed(final K key) {
+		final Triple<AtomicLong, AtomicLong, AtomicLong> cycles = this.getCycles(key);
+		cycles.key.set(0L); // accessed
+	}
+	public void mark_changed(final K key) {
+		final Triple<AtomicLong, AtomicLong, AtomicLong> cycles = this.getCycles(key);
+		cycles.key.set(0L); // accessed
+		cycles.ent.set(0L); // changed-last
+		final long first = cycles.val.get(); // changed-first
+		if (first < 0L) cycles.val.compareAndSet(first, 0L);
+	}
+	public void mark_saved(final K key) {
+		final Triple<AtomicLong, AtomicLong, AtomicLong> cycles = this.getCycles(key);
+		cycles.val.set(Long.MIN_VALUE); // changed-first
+		cycles.ent.set(Long.MIN_VALUE); // changed-last
+	}
+
+	public boolean isChanged(final K key) {
+		final Triple<AtomicLong, AtomicLong, AtomicLong> cycles = this.cycles.get(key);
+		if (cycles == null        ) return false; // not loaded
+		if (cycles.val.get() >= 0L) return true;  // changed-first
+		if (cycles.ent.get() >= 0L) return true;  // changed-last
+		return false; // unchanged
+	}
+
+	public Triple<AtomicLong, AtomicLong, AtomicLong> getCycles(final K key) {
+		final Triple<AtomicLong, AtomicLong, AtomicLong> cycles = this.cycles.get(key);
+		if (cycles == null) {
+			final AtomicLong cycle_accessed      = new AtomicLong(0L);
+			final AtomicLong cycle_changed_first = new AtomicLong(Long.MIN_VALUE);
+			final AtomicLong cycle_changed_last  = new AtomicLong(Long.MIN_VALUE);
+			final Triple<AtomicLong, AtomicLong, AtomicLong> trip =
+				new Triple<AtomicLong, AtomicLong, AtomicLong>(cycle_accessed, cycle_changed_first, cycle_changed_last);
+			final Triple<AtomicLong, AtomicLong, AtomicLong> existing = this.cycles.putIfAbsent(key, trip);
+			return (existing==null ? trip : existing);
+		}
+		return cycles;
 	}
 
 
@@ -69,30 +176,29 @@ public class CacheMap<K, V> implements Map<K, V> {
 
 
 
+	@SuppressWarnings("unchecked")
+	public K castKey(final Object obj) {
+		return (K) obj;
+	}
+
+
+
 	@Override
 	public boolean containsKey(final Object key) {
 		return this.map.containsKey(key);
 	}
 	@Override
 	public boolean containsValue(final Object match) {
-		final LinkedList<K> remove = new LinkedList<K>();
-		final Iterator<Entry<K, SoftReference<V>>> it = this.map.entrySet().iterator();
+		final Iterator<Entry<K, V>> it = this.map.entrySet().iterator();
 		while (it.hasNext()) {
-			final Entry<K, SoftReference<V>> entry = it.next();
-			final SoftReference<V> ref = entry.getValue();
-			final V val = ref.get();
+			final Entry<K, V> entry = it.next();
+			final V val = entry.getValue();
 			if (val == null) {
-				remove.add(entry.getKey());
+				if (match == null)
+					return true;
 			} else {
 				if (val.equals(match))
 					return true;
-			}
-		}
-		// cleanup
-		if (!remove.isEmpty()) {
-			for (final K key : remove) {
-				this.map.remove(key);
-				this.map_cycles.remove(key);
 			}
 		}
 		return false;
@@ -102,30 +208,25 @@ public class CacheMap<K, V> implements Map<K, V> {
 
 	@Override
 	public V get(final Object key) {
-		final SoftReference<V> ref = this.map.get(key);
-		final AtomicInteger cycles = this.map_cycles.get(key);
-		if (cycles != null)
-			cycles.set(0);
-		this.cleanup();
-		return (ref==null ? null : ref.get());
+		final V value = this.map.get(key);
+		if (value == null)
+			return null;
+		this.mark_accessed(this.castKey(key));
+		return value;
 	}
 	@Override
 	public V put(final K key, final V value) {
-		final SoftReference<V> ref = new SoftReference<V>(value);
-		final SoftReference<V> result = this.map.put(key, ref);
-		this.map_cycles.put(key, new AtomicInteger(0));
-		return (result==null ? null : result.get());
+		this.map.put(key, value);
+		this.mark_changed(key);
+		return value;
 	}
 	@Override
 	public void putAll(final Map<? extends K, ? extends V> map) {
 		final Iterator<?> it = map.entrySet().iterator();
 		while (it.hasNext()) {
 			@SuppressWarnings("unchecked")
-			final Entry<K, ? extends V> entry = (Entry<K, ? extends V>) it.next();
-			final SoftReference<V> ref = new SoftReference<V>( (V)entry.getValue() );
-			final K key = (K) entry.getKey();
-			this.map.put(key, ref);
-			this.map_cycles.put(key, new AtomicInteger(0));
+			final Entry<K, V> entry = (Entry<K, V>) it.next();
+			this.put(entry.getKey(), entry.getValue());
 		}
 	}
 
@@ -133,9 +234,7 @@ public class CacheMap<K, V> implements Map<K, V> {
 
 	@Override
 	public V remove(final Object key) {
-		final SoftReference<V> ref = this.map.remove(key);
-		this.map_cycles.remove(key);
-		return (ref==null ? null : ref.get());
+		return this.map.remove(key);
 	}
 	@Override
 	public void clear() {
@@ -146,21 +245,24 @@ public class CacheMap<K, V> implements Map<K, V> {
 
 	@Override
 	public Set<K> keySet() {
-		return null;
+		final Set<K> result = new HashSet<K>();
+		for (final K key : this.map.keySet())
+			result.add(key);
+		return result;
 	}
 	@Override
 	public Collection<V> values() {
-		return null;
+		final Set<V> result = new HashSet<V>();
+		for (final V value : this.map.values())
+			result.add(value);
+		return result;
 	}
 	public Map<K, V> getMap() {
 		final Map<K, V> result = new HashMap<K, V>();
-		final Iterator<Entry<K, SoftReference<V>>> it = this.map.entrySet().iterator();
+		final Iterator<Entry<K, V>> it = this.map.entrySet().iterator();
 		while (it.hasNext()) {
-			final Entry<K, SoftReference<V>> entry = it.next();
-			final SoftReference<V> ref = entry.getValue();
-			final V value = ref.get();
-			if (value != null)
-				result.put(entry.getKey(), value);
+			final Entry<K, V> entry = it.next();
+			result.put(entry.getKey(), entry.getValue());
 		}
 		return result;
 	}
---
+++ core/src/com/poixson/tools/CacheMapSoft.java
@@ -0,0 +1,68 @@
+package com.poixson.tools;
+
+import java.lang.ref.SoftReference;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+
+public class CacheMapSoft<K, V> extends CacheMap<K, SoftReference<V>> {
+
+
+
+	public CacheMapSoft() {
+		super();
+	}
+	public CacheMapSoft(final int cleanup_ticks,
+			final int cleanup_cycles, final int cleanup_cycles_max) {
+		super(cleanup_ticks, cleanup_cycles, cleanup_cycles_max);
+	}
+
+
+
+	@Override
+	public boolean containsValue(final Object match) {
+		final LinkedList<K> remove = new LinkedList<K>();
+		try {
+			final Iterator<Entry<K, SoftReference<V>>> it = this.map.entrySet().iterator();
+			while (it.hasNext()) {
+				final Entry<K, SoftReference<V>> entry = it.next();
+				final SoftReference<V> ref = entry.getValue();
+				final V val = ref.get();
+				if (val == null) {
+					remove.add(entry.getKey());
+				} else {
+					if (val.equals(match))
+						return true;
+				}
+			}
+		} finally {
+			if (!remove.isEmpty()) {
+				for (final K key : remove)
+					this.invalidate(key);
+			}
+		}
+		return false;
+	}
+
+
+
+	public V getref(final Object key) {
+		final SoftReference<V> ref = this.map.get(key);
+		final V value = ref.get();
+		if (value == null) {
+			this.invalidate(this.castKey(key));
+			return null;
+		} else {
+			this.mark_accessed(this.castKey(key));
+			return value;
+		}
+	}
+	public V putref(final K key, final V value) {
+		final SoftReference<V> result = this.map.put(key, new SoftReference<V>(value));
+		this.mark_changed(key);
+		return (result==null ? null : result.get());
+	}
+
+
+
+}
