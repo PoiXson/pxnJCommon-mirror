8aef81fa76a10ea0382e59a9b7eb36b5deadb80c
lorenzop <lorenzo@poixson.com>
2024-12-27 03:36:27 -0500

    rewrote to improve xURL class - merged in BuilderURL class

--- core/src/com/poixson/tools/xURL.java
+++ core/src/com/poixson/tools/xURL.java
@@ -1,101 +1,138 @@
 package com.poixson.tools;
 
-import static com.poixson.utils.MathUtils.CastInteger;
+import static com.poixson.utils.MathUtils.MAX_PORT;
+import static com.poixson.utils.StringUtils.ForceStarts;
+import static com.poixson.utils.StringUtils.NullNorm;
+import static com.poixson.utils.StringUtils.SafeString;
+import static com.poixson.utils.StringUtils.ToLower;
+import static com.poixson.utils.StringUtils.ToString;
 import static com.poixson.utils.StringUtils.ceTrim;
 import static com.poixson.utils.Utils.IsEmpty;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
-// protocol:[//[user[:password]@]host[:port]][/path]
+import com.poixson.exceptions.RequiredArgumentException;
+
+
+// protocol:[//[user[:password]@]host[:port]][/path][?key=val[&key=val]..]
 public class xURL {
 
-	public String protocol = null;
-	public String user     = null;
-	public String pass     = null;
-	public String host     = null;
-	public int    port     = -1;
-	public String path     = null;
+	public String prot = null;
+	public String user = null;
+	public String pass = null;
+	public String host = null;
+	public int    port = -1;
+	public String path = null;
+	public Map<String, String> params = new HashMap<String, String>();
 
 
 
-	public static xURL getNew() {
+	public static xURL XURL() {
 		return new xURL();
 	}
-	public static xURL getNew(final String uri) {
-		return new xURL(uri);
-	}
-	public static xURL getNew(final String protocol, final String host) {
-		return new xURL(protocol, host);
-	}
-	public static xURL getNew(final String host, final int port) {
-		return new xURL(host, port);
-	}
-	public static xURL getNew(final String protocol, final String host, final int port) {
-		return new xURL(protocol, host, port);
-	}
-	public static xURL getNew(final String protocol, final String host, final int port, final String path) {
-		return new xURL(protocol, host, port, path);
-	}
-	public static xURL getNew(final String user, final String pass,
-			final String protocol, final String host, final int port, final String path) {
-		return new xURL(protocol, user, pass, host, port, path);
-	}
-
-
 
 	public xURL() {
 	}
-	public xURL(final String uri) {
-		this();
-		this.setURI(uri);
-	}
-	public xURL(final String protocol, final String host) {
-		this();
-		this.protocol = protocol;
-		this.host     = host;
-	}
-	public xURL(final String host, final int port) {
-		this();
-		this.host     = host;
-		this.port     = port;
-	}
-	public xURL(final String protocol, final String host, final int port) {
-		this();
-		this.protocol = protocol;
-		this.host     = host;
-		this.port     = port;
-	}
-	public xURL(final String protocol, final String host, final int port, final String path) {
-		this();
-		this.protocol = protocol;
-		this.host     = host;
-		this.port     = port;
-		this.path     = path;
-	}
-	public xURL(final String user, final String pass,
-			final String protocol, final String host, final int port, final String path) {
-		this();
-		this.protocol = protocol;
-		this.user     = user;
-		this.pass     = pass;
-		this.host     = host;
-		this.port     = port;
-		this.path     = path;
+
+	@Override
+	public xURL clone() {
+		return XURL().set(this);
 	}
 
 
 
 	public xURL reset() {
-		this.protocol = null;
-		this.user     = null;
-		this.pass     = null;
-		this.host     = null;
-		this.port     = -1;
-		this.path     = null;
+		this.prot = null;
+		this.user = null;
+		this.pass = null;
+		this.host = null;
+		this.port = -1;
+		this.path = null;
+		this.params.clear();
 		return this;
 	}
 
 
 
+	public String build() {
+		if (IsEmpty(this.host)
+		&&  IsEmpty(this.path)
+		&&  IsEmpty(this.params))
+			return "";
+		final StringBuilder url = new StringBuilder();
+		// host
+		if (!IsEmpty(this.host)) {
+			if (!IsEmpty(this.prot))
+				url.append(this.prot).append("://");
+			if (!IsEmpty(this.user) || !IsEmpty(this.pass)) {
+				if (!IsEmpty(this.user)) url.append(this.user);
+				if (!IsEmpty(this.pass)) url.append(':').append(this.pass);
+				url.append('@');
+			}
+			url.append(this.host);
+			if (this.port > 0) {
+				boolean is_default = false;
+				if (!IsEmpty(this.prot)) {
+					SWITCH_PROT:
+					switch (this.prot) {
+					case "http":  if (this.port ==  80) is_default = true; break SWITCH_PROT;
+					case "https": if (this.port == 443) is_default = true; break SWITCH_PROT;
+					default: break SWITCH_PROT;
+					}
+				}
+				if (!is_default)
+					url.append(':').append(this.port);
+			}
+		}
+		// path
+		if (!IsEmpty(this.path)
+		|| (!IsEmpty(this.prot) && !IsEmpty(this.host)))
+			url.append( ForceStarts('/', SafeString(this.path)) );
+		if (!IsEmpty(this.params)) {
+			boolean first = true;
+			for (final Entry<String, String> entry : this.params.entrySet()) {
+				url.append(first ? '?' : '&');
+				url.append(entry.getKey())
+					.append('=').append(entry.getValue());
+				first = false;
+			}
+		}
+		return url.toString();
+	}
+	@Override
+	public String toString() {
+		return this.build();
+	}
+
+
+
+	// -------------------------------------------------------------------------------
+	// set url
+
+
+
+	public xURL set(final xURL url) {
+		this.prot = url.prot;
+		this.user = url.user;
+		this.pass = url.pass;
+		this.host = url.host;
+		this.port = url.port;
+		this.path = url.path;
+		this.params.clear();
+		for (final Entry<String, String> entry : url.params.entrySet())
+			this.params.put(entry.getKey(), entry.getValue());
+		return this;
+	}
+
+
+
+	public xURL set(final String url) {
+//TODO
+		return this;
+	}
+/*
 	public xURL setURI(final String uri) {
 		String buf = uri;
 		int pos;
@@ -155,50 +192,46 @@ public class xURL {
 		}
 		return this;
 	}
+*/
 
 
 
-	@Override
-	public String toString() {
-		final StringBuilder str = new StringBuilder();
-		final String protocol = this.protocol;
-		final String user     = this.user;
-		final String pass     = this.pass;
-		final String host     = this.host;
-		final int    port     = this.port;
-		final String path     = this.path;
-		if (!IsEmpty(protocol))
-			str.append(protocol).append("://");
-		if (!IsEmpty(user) || !IsEmpty(pass)) {
-			if (!IsEmpty(user)) str.append(user);
-			if (!IsEmpty(pass)) str.append(':').append(pass);
-			str.append('@');
-		}
-		str.append(host);
-		if (port > 0)       str.append(':').append(port);
-		if (!IsEmpty(path)) str.append(path);
-		return str.toString();
-	}
+	// -------------------------------------------------------------------------------
+	// set parameters
 
 
 
 	// protocol
-	public xURL setProtocol(final String protocol) {
-		this.protocol = protocol;
+	public xURL protocol(final String protocol) {
+		this.prot = ToLower(NullNorm(protocol));
 		return this;
 	}
 	public String getProtocol() {
-		return this.protocol;
+		return this.prot;
 	}
 	public boolean hasProtocol() {
-		return !IsEmpty(this.protocol);
+		return !IsEmpty(this.prot);
+	}
+
+	public xURL http() {
+		return this.protocol("http");
+	}
+	public xURL https() {
+		return this.protocol("https");
+	}
+
+	public boolean isHTTP() {
+		return "http".equals(this.prot);
+	}
+	public boolean isHTTPS() {
+		return "https".equals(this.prot);
 	}
 
 
 
 	// user
-	public xURL setUser(final String user) {
-		this.user = user;
+	public xURL user(final String user) {
+		this.user = NullNorm(user);
 		return this;
 	}
 	public String getUser() {
@@ -211,8 +244,8 @@ public class xURL {
 
 
 	// pass
-	public xURL setPass(final String pass) {
-		this.pass = pass;
+	public xURL pass(final String pass) {
+		this.pass = NullNorm(pass);
 		return this;
 	}
 	public String getPass() {
@@ -225,8 +258,8 @@ public class xURL {
 
 
 	// host
-	public xURL setHost(final String host) {
-		this.host = host;
+	public xURL host(final String host) {
+		this.host = NullNorm(host);
 		return this;
 	}
 	public String getHost() {
@@ -239,13 +272,14 @@ public class xURL {
 
 
 	// port
-	public xURL setPort(final int port) {
-		this.port = (port > 0 ? port : -1);
+	public xURL port(final int port) {
+		if (port < 1       ) this.port = -1; else
+		if (port > MAX_PORT) throw new IllegalArgumentException("Invalid port number: "+Integer.toString(port));
+		else                 this.port = port;
 		return this;
 	}
 	public int getPort() {
-		final int port = this.port;
-		return (port > 0 ? port : -1);
+		return this.port;
 	}
 	public boolean hasPort() {
 		return (this.port > 0);
@@ -254,8 +288,9 @@ public class xURL {
 
 
 	// path
-	public xURL setPath(final String path) {
-		this.path = path;
+	public xURL path(final String path) {
+		if (!IsEmpty(path))
+			this.path = ceTrim(SafeString(this.path), '/') + ForceStarts('/', path);
 		return this;
 	}
 	public String getPath() {
@@ -263,6 +298,33 @@ public class xURL {
 	}
 	public boolean hasPath() {
 		return !IsEmpty(this.path);
+	}
+
+
+
+	// params
+	public xURL param(final String key, final String val) {
+		if (IsEmpty(key)) throw new RequiredArgumentException("key");
+		if (IsEmpty(val)) throw new RequiredArgumentException("val");
+		this.params.put(key, val);
+		return this;
+	}
+	public xURL param(final String key, final Object val) {
+		return this.param(key, ToString(val));
+	}
+	public String getParam(final String key) {
+		return this.params.get(key);
+	}
+
+	public xURL setParams(final Map<String, String> params) {
+		this.params = params;
+		return this;
+	}
+	public Map<String, String> getParamsMap() {
+		return this.params;
+	}
+	public boolean hasParams() {
+		return !IsEmpty(this.params);
 	}
 
 
