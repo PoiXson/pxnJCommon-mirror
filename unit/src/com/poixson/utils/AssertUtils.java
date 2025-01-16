56534c8d38915788ba70101fc94496acee845aba
lorenzop <lorenzo@poixson.com>
2025-01-15 23:49:50 -0500

    rewrite of AssertUtils and add local IsEmpty() and ToString() functions

--- unit/src/com/poixson/utils/AssertUtils.java
+++ unit/src/com/poixson/utils/AssertUtils.java
@@ -1,13 +1,12 @@
 package com.poixson.utils;
 
-import static com.poixson.utils.StringUtils.MergeObjects;
-import static com.poixson.utils.StringUtils.ToString;
-import static com.poixson.utils.Utils.IsEmpty;
+import static com.poixson.tools.Assertions.Fail;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
-
-import org.junit.Assert;
 
 
 public final class AssertUtils {
@@ -17,24 +16,90 @@ public final class AssertUtils {
 
 	public static void AssertArrayContains(final Object[] expect, final Object[] actual) {
 		if (expect == null && actual == null) return;
-		if (expect == null) { Assert.fail(ToString(actual)); return; }
-		if (actual == null) { Assert.fail(ToString(expect)); return; }
-		final List<Object> actualList = new ArrayList<Object>();
+		if (expect == null) { Fail("actual is null"); return; }
+		if (actual == null) { Fail("expect is null"); return; }
+		final List<Object> actual_list = new ArrayList<Object>();
 		for (int index=0; index<actual.length; index++)
-			actualList.add(actual[index]);
+			actual_list.add(actual[index]);
 		final List<Object> foundList = new ArrayList<Object>();
 		for (int index=0; index<expect.length; index++) {
 			final Object obj = expect[index];
-			if (!actualList.contains(obj)) {
-				Assert.fail("Entry not in actual array: "+ToString(obj));
+			if (!actual_list.contains(obj)) {
+				Fail("Entry not in actual array: "+ToString(obj));
 				return;
 			}
 			foundList.add(obj);
 		}
 		for (final Object obj : foundList)
-			actualList.remove(obj);
-		if (!IsEmpty(actualList))
-			Assert.fail("Entries not in expected array: "+MergeObjects(", ", actualList));
+			actual_list.remove(obj);
+		if (!IsEmpty(actual_list))
+			Fail("Entries not in expected array: "+MergeObjects(", ", actual_list));
+	}
+
+
+
+	// -------------------------------------------------------------------------------
+
+
+
+	public static boolean IsEmpty(final Object[] array) {
+		if (array == null) return true;
+		return (array.length == 0);
+	}
+	public static boolean IsEmpty(final Collection<?> collect) {
+		if (collect == null) return true;
+		return (collect.size() == 0);
+	}
+
+
+
+	public static String ToString(final Object obj) {
+		if (obj == null) return null;
+		if (obj.getClass().isArray()) {
+			final StringBuilder result = new StringBuilder();
+			for (final Object o : (Object[]) obj) {
+				if (!result.isEmpty())
+					result.append(' ');
+				result.append('{').append( ToString(o) ).append('}');
+			}
+			return result.toString();
+		}
+		if (obj instanceof Boolean  ) return ( ((Boolean)obj).booleanValue() ? "<TRUE>" : "<false>" );
+		if (obj instanceof String   ) return (String) obj;
+		if (obj instanceof Integer  ) return ((Integer) obj).toString();
+		if (obj instanceof Long     ) return ((Long)    obj).toString();
+		if (obj instanceof Double   ) return ((Double)  obj).toString();
+		if (obj instanceof Float    ) return ((Float)   obj).toString();
+		if (obj instanceof Exception) return ExceptionToString((Exception) obj);
+		// unknown object
+		return obj.toString();
+	}
+	public static String ExceptionToString(final Throwable e) {
+		if (e == null) return null;
+		final StringWriter writer = new StringWriter(256);
+		e.printStackTrace(new PrintWriter(writer));
+		try {
+			return writer.toString().trim();
+		} finally {
+			try {
+				writer.close();
+			} catch (Exception ignore) {}
+		}
+	}
+
+
+
+	public static String MergeObjects(final String delim, final Object...array) {
+		final StringBuilder buf = new StringBuilder();
+		// merge with delim
+		for (final Object entry : array) {
+			if (entry != null) {
+				if (!buf.isEmpty())
+					buf.append(delim);
+				buf.append(ToString(entry));
+			}
+		}
+		return buf.toString();
 	}
 
 
