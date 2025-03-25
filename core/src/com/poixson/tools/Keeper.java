3cb4c8f82d7c0e6985a511e659d656f875ea8fa6
lorenzop <lorenzo@poixson.com>
2025-03-25 00:51:02 -0400

    cleanup and improvements to Keeper class

--- core/src/com/poixson/tools/Keeper.java
+++ core/src/com/poixson/tools/Keeper.java
@@ -1,6 +1,5 @@
 package com.poixson.tools;
 
-import java.util.Iterator;
 import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -9,57 +8,33 @@ import com.poixson.exceptions.RequiredArgumentException;
 
 public class Keeper {
 
-	private static final AtomicReference<Keeper> instance =
-			new AtomicReference<Keeper>(null);
+	private static final AtomicReference<Keeper> instance = new AtomicReference<Keeper>(null);
 
-	private static final CopyOnWriteArraySet<Object> holder =
-			new CopyOnWriteArraySet<Object>();
+	private static final CopyOnWriteArraySet<Object> holder = new CopyOnWriteArraySet<Object>();
+
+
+
+	static {
+		instance.compareAndSet(null, new Keeper());
+	}
+
+	private Keeper() {
+	}
 
 
 
 	public static Keeper Get() {
-		if (instance.get() == null) {
-			// new instance
-			final Keeper keeper = new Keeper();
-			if (instance.compareAndSet(null, keeper))
-				return keeper;
-		}
 		return instance.get();
 	}
-
-
 
 	public static void Add(final Object obj) {
 		if (obj == null) throw new RequiredArgumentException("obj");
 		holder.add(obj);
 	}
 
-
-
 	public static void Remove(final Object obj) {
 		if (obj == null) throw new RequiredArgumentException("obj");
 		holder.remove(obj);
-	}
-	@Deprecated
-	public static void RemoveAll() {
-		holder.clear();
-	}
-	@Deprecated
-	public static int RemoveAll(final Class<? extends Object> clss) {
-		if (holder.isEmpty())
-			return 0;
-		int count = 0;
-		final String expect = clss.getName();
-		final Iterator<Object> it = holder.iterator();
-		while (it.hasNext()) {
-			final Object obj = it.next();
-			final String actual = obj.getClass().getName();
-			if (expect.equals(actual)) {
-				count++;
-				Remove(obj);
-			}
-		}
-		return count;
 	}
 
 
